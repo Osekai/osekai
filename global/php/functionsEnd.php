714ee0cc9eff6c817d0bbbbfc2481eb7cca3f22d
Tanza3D <hubz@hubza.co.uk>
2022-11-28 17:40:14 +0000

    cleaner

--- global/php/functionsEnd.php
+++ global/php/functionsEnd.php
@@ -19,15 +19,15 @@ $tooltipText = "This debug panel is so that you, and our team, can make sure Ose
 //$tooltipText .= "\n  |  Aborted Session Saves: Amount of times a session has attempted to be saved, but no data has changed.";
 
 
-    $gitBasePath = $_SERVER['DOCUMENT_ROOT']  . '/.git'; // e.g in laravel: base_path().'/.git';
+$gitBasePath = $_SERVER['DOCUMENT_ROOT']  . '/.git'; // e.g in laravel: base_path().'/.git';
 
-    $gitStr = file_get_contents($gitBasePath.'/HEAD');
-    $gitBranchName = rtrim(preg_replace("/(.*?\/){2}/", '', $gitStr));                                                                                            
-    $gitPathBranch = $gitBasePath.'/refs/heads/'.$gitBranchName;
-    $gitHash = file_get_contents($gitPathBranch);
-    $gitDate = date(DATE_ATOM, filemtime($gitPathBranch));
+$gitStr = file_get_contents($gitBasePath . '/HEAD');
+$gitBranchName = rtrim(preg_replace("/(.*?\/){2}/", '', $gitStr));
+$gitPathBranch = $gitBasePath . '/refs/heads/' . $gitBranchName;
+$gitHash = file_get_contents($gitPathBranch);
+$gitDate = date(DATE_ATOM, filemtime($gitPathBranch));
 
-    echo "version date: ".$gitDate."<br>branch: ".$gitBranchName."<br> commit: ".$gitHash;                                                       
+echo "version date: " . $gitDate . "<br>branch: " . $gitBranchName . "<br> commit: " . $gitHash;
 ?>
 <meta charset="utf-8">
 <div class="debug">
