569b14f6542e517f126c3c9be571cd633bcf6d82
jiniux <53437303+jiniux@users.noreply.github.com>
2022-12-07 15:34:42 +0100

    Refactor goals.php and add extra checks

--- profiles/api/goals.php
+++ profiles/api/goals.php
@@ -1,24 +1,84 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . "/global/php/functions.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/global/php/api_controller.php");
 
-if(isset($_POST['Value']) && !isRestricted()) {
-    if(isset($_SESSION['osu']['id'])) {
-        Database::execOperation("INSERT IGNORE INTO Goals (UserID, Value, Type, Gamemode, CreationDate) VALUES (?, ?, ?, ?, NOW())", "iiss", array($_SESSION['osu']['id'], $_POST['Value'], $_POST['Type'], $_POST['Gamemode']));
-        echo json_encode("Success!");
+function get_goal($userId, $value, $type, $gamemode)
+{
+    return Database::execSelectFirstOrNull(
+        "SELECT * FROM Goals WHERE UserID = ? AND Value = ? AND Type = ? and Gamemode = ?",
+        "isss",
+        [$userId, $value, $type, $gamemode]
+    );    
+}
+
+define("VALUE_REGEX", "/^\d+(?:\.\d{1,2})?$/");
+define("VALID_GAMEMODES", array("taiko", "mania", "osu", "fruits"));
+define("VALID_TYPES", array("PP", "Rank", "Country Rank", "Medals", "% Medals", "SS Count", "Badges", "Level", "Ranked Score"));
+
+class GoalsApiController extends ApiController
+{
+    protected function post(): Response {
+        if (!isset($_SESSION['osu']['id']) || isRestricted())
+            return new UnauthorizedJsonResponse();
+
+        if(isset($_POST['Value'])) {
+            $value = $_POST['Value'];
+            
+            if (!preg_match(VALUE_REGEX, $value))
+                return new BadRequestJsonResponse("Invalid Value format");
+
+            $userId = $_SESSION['osu']['id'];
+            $type = $_POST['Type'];
+
+            if (!in_array($type, VALID_TYPES))
+                return new BadRequestJsonResponse("Invalid Type");
+            
+            $gamemode = $_POST['Gamemode'];
+
+            if (!in_array($gamemode, VALID_GAMEMODES))
+                return new BadRequestJsonResponse("Invalid Gamemode");
+
+            if (get_goal($userId, $value, $type, $gamemode) != null)
+                return new BadRequestJsonResponse("Goal already exists");
+
+            Database::execOperation(
+                "INSERT INTO Goals (UserID, Value, Type, Gamemode, CreationDate) VALUES (?, ?, ?, ?, NOW())", 
+                "isss", 
+                array($_SESSION['osu']['id'], $value, $_POST['Type'], $_POST['Gamemode'])
+            );
+
+            return new JsonResponse("Success!");
+        }
+
+        if(isset($_POST['GoalID'])) {
+            if (!filter_var($_POST['GoalID'], FILTER_VALIDATE_INT))
+                return new BadRequestJsonResponse("Invalid GoalID");
+            
+            Database::execOperation(
+                "DELETE FROM Goals WHERE ID = ? AND UserID = ?", 
+                "ii", 
+                array($_POST['GoalID'], $_SESSION['osu']['id'])
+            );
+
+            return new JsonResponse("Success!");
+        }
+
+        if(isset($_POST['ClaimID'])) {
+            if (!filter_var($_POST['ClaimID'], FILTER_VALIDATE_INT))
+                return new BadRequestJsonResponse("Invalid ClaimID");
+
+            Database::execOperation(
+                "UPDATE Goals SET Claimed = NOW() WHERE ID = ((? - Value) / 100) AND UserID = ?", 
+                "ii", 
+                array($_POST['ClaimID'], $_SESSION['osu']['id'])
+            );
+            return new JsonResponse("Success!");
+        }
+
+        return new NotImplementedResponse;
     }
 }
 
-if(isset($_POST['GoalID']) && !isRestricted()) {
-    if(isset($_SESSION['osu']['id'])) {
-        Database::execOperation("DELETE FROM Goals WHERE ID = ? AND UserID = ?", "ii", array($_POST['GoalID'], $oSession['osu']['id']));
-        echo json_encode("Success!");
-    }
-}
+(new GoalsApiController())->execute();
 
-if(isset($_POST['ClaimID']) && !isRestricted()) {
-    if(isset($_SESSION['osu']['id'])) {
-        Database::execOperation("UPDATE Goals SET Claimed = NOW() WHERE ID = ((? - Value) / 100) AND UserID = ?", "ii", array($_POST['ClaimID'], $oSession['osu']['id']));
-        echo json_encode("Success!");
-    }
-}
 ?>
