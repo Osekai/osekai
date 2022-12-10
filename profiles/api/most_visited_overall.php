dc6d2b63ef4c4e9342cf5aca072c523198b8d38b
jiniux <jiniux@users.noreply.github.com>
2022-12-10 14:19:56 +0100

    Refactor most_visited.php. Use SqlTimeSpecifier in getMostVisited.

--- profiles/api/most_visited_overall.php
+++ profiles/api/most_visited_overall.php
@@ -4,13 +4,13 @@ include_once($_SERVER['DOCUMENT_ROOT'] . "/global/php/functions.php");
 api_controller_base_classes();
 profiles_visited_repository();
 
-class MostVisitedApiController extends ApiController
+class MostVisitedOverallApiController extends ApiController
 {
     public function get(): ApiResult {
         return new OkApiResult(ProfilesVisitedRepository::getMostVisited());
     }
 }
 
-ApiControllerExecutor::execute(new MostVisitedApiController, new JsonApiResultSerializer);
+ApiControllerExecutor::execute(new MostVisitedOverallApiController, new JsonApiResultSerializer);
 
 ?>
