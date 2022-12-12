fc6136db9a5d5d64bb61efdcbb1261fab6163229
Tanza3D <hubz@hubza.co.uk>
2022-12-12 23:38:04 +0000

    Add new font for large bodies of text

--- global/php/functions.php
+++ global/php/functions.php
@@ -316,6 +316,7 @@ function font()
     // imports font
 
     echo '<link rel="preload" rel="preconnect" href="https://fonts.gstatic.com"><link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">';
+    echo '<link href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">';
 }
 
 function navbar()
