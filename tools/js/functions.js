e551a60eb09aa7247daee55fa7ecf46ffbd84bf2
Tanza3D <hubz@hubza.co.uk>
2022-12-30 18:35:42 +0000

    that

--- tools/js/functions.js
+++ tools/js/functions.js
@@ -1,3 +1,3 @@
 function useCustomThemeForTool(accent, accentDark) {
-    document.getElementsByClassName("tools__tool")[0].innerHTML += `<style>.tools__tool {` + generateCustomThemeVars(accent, accentDark) + `}</style>`
+    document.getElementsByClassName("tools__tool")[0].innerHTML += `<style>.tools__tool {${generateCustomThemeVars(accent, accentDark)}}</style>`
 }
