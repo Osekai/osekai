c22a4f7bac48ae9b1f09e55f9da6ac77a5dc64d1
Tanza3D <hubz@hubza.co.uk>
2023-01-15 14:18:01 +0000

    Move tools to database

--- tools/tools.php
+++ tools/tools.php
@@ -1,13 +1,2 @@
 <?php
-$tools = [];
-
-function addTool($key, $name, $path) {
-    global $tools;
-    $tools[] = ["key" => $key, "name" => $name, "path" => $path, "urlpath" => "/tools/".$path];
-}
-
-addTool("ppcalc", "PP Calculator", "src/ppcalc");
-addTool("medal-percentage-calc", "Medal Percentage Calculator", "src/medal_percentage_calc");
-addTool("comparer", "Comparer", "src/comparer");
-addTool("medal-name-quiz", "Medal Name Quiz", "src/medal_name_quiz");
-addTool("stdev-pp-calc", "Standard Deviated PP Calculator", "src/stdev_pp_calc");
+$tools = Database::execSimpleSelect("SELECT * FROM Tools");
