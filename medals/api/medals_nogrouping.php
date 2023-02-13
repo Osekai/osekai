439b25c735e8d3485ae68f1e58b3db3233c0aaed
EXtremeExploit <pedro.montes.alcalde@gmail.com>
2023-02-13 19:29:44 -0300

    Make medals_nogrouping endpoint return JSON content type

--- medals/api/medals_nogrouping.php
+++ medals/api/medals_nogrouping.php
@@ -3,4 +3,5 @@ include_once($_SERVER['DOCUMENT_ROOT'] . "/global/php/functions.php");
 
 $medals = Database::execSimpleSelect("SELECT * FROM Medals");
 
+header('Content-Type: application/json; charset=utf-8');
 echo json_encode($medals);
