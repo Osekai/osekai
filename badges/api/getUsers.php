b2aec3d881b08e5bd4d950d96023406ae3120f28
EXtremeExploit <pedro.montes.alcalde@gmail.com>
2023-03-24 10:54:49 -0300

    badges: Request v2 user when not in rankings

--- badges/api/getUsers.php
+++ badges/api/getUsers.php
@@ -6,9 +6,19 @@ $users = json_decode($badges[0]['users']);
 
 $nu;
 
-for ($i = 0; $i < count($users); $i++)
-{
-    $nu[$i] = Database::execSelect("SELECT * FROM Ranking WHERE id = ?", "i", array($users[$i]))[0];
+for ($i = 0; $i < count($users); $i++) {
+    $rankingUser = Database::execSelect("SELECT * FROM Ranking WHERE id = ?", "i", array($users[$i]));
+    if ($rankingUser != null)
+        $nu[$i] = $rankingUser[0];
+    else {
+        $apiuser = v2_getUser($users[$i], "osu", false, false);
+        if ($apiuser != null) // check if user is restricted
+            $decodedApiUser = json_decode($apiuser, true);
+
+        $nu[$i] = null;
+        $nu[$i]["id"] = $users[$i];
+        $nu[$i]["name"] = $decodedApiUser["username"];
+    }
 }
 
 echo json_encode($nu);
