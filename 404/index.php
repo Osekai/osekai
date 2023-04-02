87457c83a36fcc8806ece19abd13777af8f04050
MegaMix_Craft <43725099+minusQuantumNeko@users.noreply.github.com>
2023-04-03 00:25:08 +0300

    Add more responses, nya~
    nya~

--- 404/index.php
+++ 404/index.php
@@ -38,7 +38,11 @@ $texts = [
     "oopsies, this page doesn't exist >w<",
     "oops, that page doesn't exist!",
     "you seem to be lost!",
-    "where've you ended up?"
+    "where've you ended up?",
+    "Uh oh, looks like we hit a dead end!",
+    "Oh no! We couldn't find what you were looking for!",
+    "It appears you've stumbled upon a page that doesn't exist!",
+    "Oops, looks like we took a wrong turn somewhere!"
 ];
 $subtexts = [
     "let's get you back on track.",
@@ -46,7 +50,11 @@ $subtexts = [
     "pick a place from our nice selection below!",
     "let's get you back home... or another app, your choice:",
     "let's get you back on track!",
-    "i'm trying my best to figure out where you are, but i just have no clue..."
+    "I'm trying my best to figure out where you are, but I just have no clue...",
+    "Let's not give up hope just yet. Take a look at some of our recommended pages below.",
+    "Don't worry, we've got your back. Explore some of our popular sections below and find your way back on track.",
+    "Let's try not to get too upset about it. Check out some of our other pages below and see if we can find what you're looking for.",
+    "Let's get you back on the right path with some of our suggested pages below."
 ];
 ?>
 
