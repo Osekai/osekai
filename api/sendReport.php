367c98d8bb31a040ffb473d75de4d0edb5a6a363
Tanza3D <hubsterhub@gmail.com>
2023-05-15 14:54:09 +0100

    new reports system

--- api/sendReport.php
+++ api/sendReport.php
@@ -1,9 +1,59 @@
 <?php
-include("../config.php");
-$url = "http://".VPS_IP.":8120/report?";
-foreach ($_GET as $key => $value) {
-    $url .= $key . "=" . $value . "&";
+require_once($_SERVER['DOCUMENT_ROOT'] . "/global/php/functions.php");
+
+if(!loggedin()) {
+    echo json_encode(["error" => "not logged in"]);
+    exit;
 }
-$url = substr($url, 0, -1);
-echo "<p>$url</p>";
-file_get_contents($url);
+
+$reporterId = $_SESSION['osu']['id'];
+$type = $_REQUEST['reportType'];
+$status = 1;
+$text = $_REQUEST['reportText'];
+$link = $_REQUEST['reportLink'];
+$referenceId = $_REQUEST['refId'];
+
+$typeMapping = ["Beatmap", "Comment", "Bug"];
+
+print_r($type);
+
+Database::execOperation("INSERT INTO `Reports` (`ReporterId`, `Type`, `Status`, `Text`, `Link`, `ReferenceId`, `Date`)
+VALUES (?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP());", "iiissi", [$reporterId, $type, $status, $text, $link, $referenceId]);
+$id = Database::execSimpleSelect("SELECT * FROM Reports ORDER BY Id DESC LIMIT 1")[0]['Id'];
+
+$json_data = json_encode([
+    "username" => $reporterId,
+    "avatar_url" => "https://a.ppy.sh/" . $reporterId,
+    "content" => "",
+    "tts" => false,
+    "embeds" => [
+      [
+        "id" => 471418559,
+        "description" => $text,
+        "fields" => [],
+        "author" => [
+          "name" => "Report - " . $typeMapping[$type],
+          "url" => "https://osekai.net/admin/something/whatever" . $id,
+          "icon_url" => "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Exclamation_Circle_Red.svg/1024px-Exclamation_Circle_Red.svg.png"
+        ],
+        "title" => "On page " . $link,
+        "url" => $link,
+        "color" => 16737962
+      ]
+    ],
+    "components" => [],
+    "actions" => []
+], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+
+
+$ch = curl_init( MODERATION_WEBHOOK );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+curl_setopt( $ch, CURLOPT_POST, 1);
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $json_data);
+curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt( $ch, CURLOPT_HEADER, 0);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+$response = curl_exec( $ch );
+echo $response;
+curl_close( $ch );
--- medals/js/functions.js
+++ medals/js/functions.js
@@ -555,7 +555,7 @@ function requestBeatmaps(strKey, strValue, strUrl) {
 
 function reportBeatmap(beatmapId, beatmapName) {
 
-    doReport("beatmap", beatmapId, { "beatmapName": beatmapName });
+    doReport("beatmap", beatmapId);
 }
 
 var colNotes = {};
