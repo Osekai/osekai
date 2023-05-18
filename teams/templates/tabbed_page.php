eb35d3524301ef9234788f6473fa6af172559c87
Tanza3D <hubsterhub@gmail.com>
2023-05-18 14:27:07 +0100

    no more

--- teams/templates/tabbed_page.php
+++ teams/templates/tabbed_page.php
@@ -4,19 +4,8 @@
             <i class="fas fa-home"></i>
         </div>
         <div class="teams__page-navigation-pages">
-            <?php
-            if (isset($ref_page['pages'])) {
-                foreach ($ref_page['pages'] as $_page) {
-                    $classes = "";
-                    if($ref_page_inner == $_page) {
-                        $classes = "teams__page-navigation-item-active";
-                    }
-                    echo "<a class=\"teams__page-navigation-item ".$classes."\" href=\"/teams/".$ref_page['name']."/".$_page['name']."\">";
-                    echo $_page['display_name'];
-                    echo "</a>";
-                }
-            }
-            ?>
+            
+            
         </div>
         <div class="teams__page-navigation-menu">
             <i class="fas fa-circle"></i>
