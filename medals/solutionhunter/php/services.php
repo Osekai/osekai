1f701f604084f488b725546e167b7cf0b366a075
jiniux <53437303+jiniux@users.noreply.github.com>
2023-08-13 11:41:21 +0200

    fix: remove Database::wrapInTransaction

--- medals/solutionhunter/php/services.php
+++ medals/solutionhunter/php/services.php
@@ -18,48 +18,47 @@ final class SolutionTrackerService {
     private function __construct() {}
 
     public static function addSolutionIdea(int $submitterId, int $medalId, SolutionTrackerText $text): AddSolutionIdeaResult {
-        return Database::wrapInTransaction(function() use ($submitterId, $medalId, $text) {
-            $queryResult = Database::execSelectFirstOrNull(
-                "SELECT EXISTS (SELECT * FROM SolutionTracker WHERE UserId = ? AND Type = 1) as `solutionAlreadySubmitted`, " . 
-                "EXISTS (SELECT * FROM Medals WHERE medalid = ? AND solutiontrackerenabled = 1) as `solutionTrackerEnabledForMedal`;", "ii", [$submitterId, $medalId]);
-                
-            $solutionAlreadySubmitted = $queryResult['solutionAlreadySubmitted'];
-            $solutionTrackerEnabledForMedal = $queryResult['solutionTrackerEnabledForMedal'];
+        $queryResult = Database::execSelectFirstOrNull(
+            "SELECT EXISTS (SELECT * FROM SolutionTracker WHERE UserId = ? AND Type = 1) as `solutionAlreadySubmitted`, " .
+            "EXISTS (SELECT * FROM Medals WHERE medalid = ? AND solutiontrackerenabled = 1) as `solutionTrackerEnabledForMedal`;", "ii", [$submitterId, $medalId]);
 
-            if ($solutionAlreadySubmitted)
-                return AddSolutionIdeaResult::UserAlreadySubmittedSolutionIdea;
+        $solutionAlreadySubmitted = $queryResult['solutionAlreadySubmitted'];
+        $solutionTrackerEnabledForMedal = $queryResult['solutionTrackerEnabledForMedal'];
 
-            if (!$solutionTrackerEnabledForMedal)
-                return AddSolutionIdeaResult::SolutionTrackerNotEnabledForMedal;
+        if ($solutionAlreadySubmitted)
+            return AddSolutionIdeaResult::UserAlreadySubmittedSolutionIdea;
 
-            Database::execOperation("INSERT INTO SolutionTracker (`MedalId`, `UserId`, `Text`, `Type`, `Status`) VALUES (?, ?, ?, 1, 0)", 
-                "iis", 
-                [$medalId, $submitterId, $text->asString()]);
+        if (!$solutionTrackerEnabledForMedal)
+            return AddSolutionIdeaResult::SolutionTrackerNotEnabledForMedal;
 
-            return AddSolutionIdeaResult::Success;
-        });
+        Database::execOperation("INSERT INTO SolutionTracker (`MedalId`, `UserId`, `Text`, `Type`, `Status`) VALUES (?, ?, ?, 1, 0)",
+            "iis",
+            [$medalId, $submitterId, $text->asString()]);
+
+        return AddSolutionIdeaResult::Success;
     }
 
+    /**
+     * @throws Exception
+     */
     public static function addSolutionAttempt(int $submitterId, int $medalId, SolutionTrackerText $text): AddSolutionAttempt {
-        return Database::wrapInTransaction(function() use ($submitterId, $medalId, $text) {
-            $queryResult = Database::execSelectFirstOrNull(
-                "SELECT EXISTS (SELECT * FROM SolutionTracker WHERE UserId = ? AND Type = 2) as `solutionAlreadySubmitted`, " . 
-                "EXISTS (SELECT * FROM Medals WHERE medalid = ? AND solutiontrackerenabled = 1) as `solutionTrackerEnabledForMedal`;", "ii", [$submitterId, $medalId]);
-                
-            $solutionAlreadySubmitted = $queryResult['solutionAlreadySubmitted'];
-            $solutionTrackerEnabledForMedal = $queryResult['solutionTrackerEnabledForMedal'];
+        $queryResult = Database::execSelectFirstOrNull(
+            "SELECT EXISTS (SELECT * FROM SolutionTracker WHERE UserId = ? AND Type = 2) as `solutionAlreadySubmitted`, " .
+            "EXISTS (SELECT * FROM Medals WHERE medalid = ? AND solutiontrackerenabled = 1) as `solutionTrackerEnabledForMedal`;", "ii", [$submitterId, $medalId]);
 
-            if ($solutionAlreadySubmitted)
-                return AddSolutionAttempt::UserAlreadySubmittedSolutionAttempt;
+        $solutionAlreadySubmitted = $queryResult['solutionAlreadySubmitted'];
+        $solutionTrackerEnabledForMedal = $queryResult['solutionTrackerEnabledForMedal'];
 
-            if (!$solutionTrackerEnabledForMedal)
-                return AddSolutionAttempt::SolutionTrackerNotEnabledForMedal;
+        if ($solutionAlreadySubmitted)
+            return AddSolutionAttempt::UserAlreadySubmittedSolutionAttempt;
 
-            Database::execOperation("INSERT INTO SolutionTracker (`MedalId`, `UserId`, `Text`, `Type`, `Status`) VALUES (?, ?, ?, 2, 0)", 
-                "iis", 
-                [$medalId, $submitterId, $text->asString()]);
+        if (!$solutionTrackerEnabledForMedal)
+            return AddSolutionAttempt::SolutionTrackerNotEnabledForMedal;
 
-            return AddSolutionIdeaResult::Success;
-        });
+        Database::execOperation("INSERT INTO SolutionTracker (`MedalId`, `UserId`, `Text`, `Type`, `Status`) VALUES (?, ?, ?, 2, 0)",
+            "iis",
+            [$medalId, $submitterId, $text->asString()]);
+
+        return AddSolutionAttempt::Success;
     }
 }
