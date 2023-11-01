69e8034596e85c96eb1da0ab956381df4264df3b
Tanza3D <hubsterhub@gmail.com>
2023-11-01 19:10:05 +0000

    Revert "slighlty adjust"
    This reverts commit 0d3dc3badc2b006761d779283db72fead225f589.

--- global/php/functions.php
+++ global/php/functions.php
@@ -294,7 +294,7 @@ function css()
 
     // set the accent
 
-    $apps[$app]['color_dark'] = "60,24,10";
+    $apps[$app]['color_dark'] = "60,20,10";
     $apps[$app]['color'] = "180,60,10";
     if (isset($accent_override)) {
         $apps[$app]['color_dark'] = implode(",", $accent_override[1]);
