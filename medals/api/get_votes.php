debbcf4ed485afdccb213c3020f4eb57c015acea
Tanza3D <hubsterhub@gmail.com>
2023-12-03 13:38:51 +0000

    let me in

--- medals/api/get_votes.php
+++ medals/api/get_votes.php
@@ -1,5 +1,6 @@
 <?php
 // for external display
+header("Access-Control-Allow-Origin: *");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/global/php/functions.php");
 if($_GET['key'] != LANG_UPDATE_KEY) exit;
 echo json_encode(Database::execSimpleSelect("SELECT * FROM MedalVote"));
