958dcae5cd551f15271f2a2a07fffeaa4167e87e
Tanza3D <hubsterhub@gmail.com>
2023-12-03 13:33:48 +0000

    fix apis

--- medals/api/get_votes.php
+++ medals/api/get_votes.php
@@ -1,5 +1,5 @@
 <?php
 // for external display
-if($_GET['key'] != LANG_UPDATE_KEY) exit;
 require_once($_SERVER['DOCUMENT_ROOT'] . "/global/php/functions.php");
+if($_GET['key'] != LANG_UPDATE_KEY) exit;
 echo json_encode(Database::execSimpleSelect("SELECT * FROM MedalVote"));
