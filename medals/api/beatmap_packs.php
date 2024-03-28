658472e24e069d45f8f73c6095c6170e74f2377f
Your Name <you@example.com>
2024-03-28 19:53:18 +0000

    Temporarily block beatmap packs

--- medals/api/beatmap_packs.php
+++ medals/api/beatmap_packs.php
@@ -1,4 +1,5 @@
 <?php
+exit;
 ini_set('memory_limit','2048M');
 // Tanza 2023-05-13
 // ? i'm so sorry for this mess, but it takes 118ms to
--- medals/api/get_beatmap_pack_count.php
+++ medals/api/get_beatmap_pack_count.php
@@ -1,4 +1,5 @@
 <?php
+exit;
 require_once($_SERVER['DOCUMENT_ROOT'] . "/global/php/functions.php");
 
 error_reporting(E_ERROR);
